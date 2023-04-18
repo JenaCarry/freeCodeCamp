5d9b766c0f623d954d5d01ca8f61a8fe4d128f2e
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-04-18 15:11:28 -0300

    Update embaralhar.js

--- javascritp-site/embaralhar.js
+++ javascritp-site/embaralhar.js
@@ -1,13 +1,13 @@
 function embaralharPalavra(msg) {
-    const palavraOrdem = msg.split("");
-    const letrasEmbaralhadas = [];
+  const palavraOrdem = msg.split("");
+  const letrasEmbaralhadas = [];
 
-    for (let i = 0; palavraOrdem.length; i++) {
-        const random = Math.floor(Math.random() * palavraOrdem.length);
-        const letras = palavraOrdem.splice(random, 1)[0];
-        letrasEmbaralhadas.push(letras);
-    }
-    return letrasEmbaralhadas.join("");
+  for (let i = 0; palavraOrdem.length; i++) {
+    const random = Math.floor(Math.random() * palavraOrdem.length);
+    const letras = palavraOrdem.splice(random, 1)[0];
+    letrasEmbaralhadas.push(letras);
+  }
+  return letrasEmbaralhadas.join("");
 }
 
 console.log(embaralharPalavra("JavaScript"));
