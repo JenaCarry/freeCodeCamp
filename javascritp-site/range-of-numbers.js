7ec4724c1e965d696dfc6a990c615579a4f2e307
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-04-19 02:23:05 -0300

    a

--- javascritp-site/range-of-numbers.js
+++ javascritp-site/range-of-numbers.js
@@ -9,3 +9,15 @@ function rangeOfNumbers(startNum, endNum) {
 }
 
 console.log(rangeOfNumbers(1, 5));
+
+function rangeOfNumbers2(startNum, endNum) {
+  if (endNum < startNum) {
+    return [];
+  } else {
+    const array = rangeOfNumbers2(startNum, endNum - 1);
+    array.push(endNum);
+    return array;
+  }
+}
+
+console.log(rangeOfNumbers2(1, 5));
