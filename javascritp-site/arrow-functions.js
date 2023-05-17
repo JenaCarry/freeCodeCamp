2cf63c498eff331b87445e3321c1704f474db556
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-05-17 20:24:20 -0300

    .

--- javascritp-site/arrow-functions.js
+++ javascritp-site/arrow-functions.js
@@ -1,3 +1,14 @@
 const magic = () => new Date().getFullYear();
 
 console.log(magic());
+
+const myConcat = (arr1, arr2) => {
+  return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+const increment = (number, value = 1) => number + value;
+
+console.log(increment(1));
+console.log(increment(1, 2));
